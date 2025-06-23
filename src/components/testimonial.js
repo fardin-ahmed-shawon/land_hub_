acdbfdb1c2609a68ebe8a3e4fed896dcfbf56add
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-06-23 23:30:40 +0600

    up to date

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -1,7 +1,14 @@
 function Testimonial() {
     return (
       <>
-        Testimonial
+        <div className="container-xxl py-5">
+            <div className="container">
+                <div className="text-center mx-auto mb-5 wow fadeInUp" style={{maxWidth: '600px'}}>
+                    <h1 className="mb-3">Our Clients Say!</h1>
+                    <p>Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
+                </div>
+            </div>
+        </div>
       </>
     );
 }
